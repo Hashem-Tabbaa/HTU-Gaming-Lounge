9b222ede5c082ef20069467e8ff61952067ce828
hashem <74487162+Hashem-Tabbaa@users.noreply.github.com>
2022-12-19 22:10:49 +0300

    database

--- database/migrations/2022_12_08_160733_create_users_table.php
+++ database/migrations/2022_12_08_160733_create_users_table.php
@@ -25,6 +25,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('otp')->nullable();
             $table->integer('number_of_reservations')->default(0);
+            $table->string('password_reset_token ')->nullable();
             $table->timestamps();
         });
     }
